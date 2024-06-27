05e9b4ff80cf40ce5442b842dbbe5122967382e0
Ayomicom991 <85806018+Ayomicom991@users.noreply.github.com>
2024-06-27 16:50:59 +0000

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[{
+            hostname: "lh3.googleusercontent.com",
+            protocol: "https"
+        }]
+    }
+};
 
 export default nextConfig;
